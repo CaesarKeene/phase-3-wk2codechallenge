4ff83326b3ec201c683e38d689ec79b5e3a329a0
Caesar <caesar.keene@gmail.com>
2024-01-12 14:19:37 +0300

    final

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from customer import Customer
 from restaurant import Restaurant
 from review import Review
 
+# testing 
+
 Jeremy = Customer("Jeremy", "Keene")
 Alice = Customer("Alice", "Smith")
 Bob = Customer("Bob", "Johnson")
@@ -14,9 +16,11 @@ review2 = Review(Alice, McDonalds, 5)
 review3 = Review(Bob, PizzaHut, 3)
 review4 = Review(Jeremy, PizzaHut, 2)
 
+# testing the names
 print(Jeremy.full_name())
 print(Alice.full_name())
 print(Bob.full_name())
 
+# testing the star ratings
 print(McDonalds.average_star_rating()) 
 print(PizzaHut.average_star_rating())
